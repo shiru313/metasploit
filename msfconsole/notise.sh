063ceb92fd5dd5f51e5e2b1d12fb2e0e94c28d14
SHIRU n00b <72118394+shiru313@users.noreply.github.com>
2021-08-24 13:29:25 +0530

    Add files via upload

---
+++ msfconsole/notise.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+GREEN='\e[32m'
+RED='\e[31m'
+echo
+echo
+echo    #coder by noob hacker
+echo
+echo
+echo
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+sleep 1.0
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"    #SHIRFAN MLP NOOB HACKER
+#WA.ME/+919895967245
+sleep 1.0
+echo -e "${RED}All you have to do is type [msfconsole] to start from scratch"
+slepp 1.0
+echo -e "${GREEN}All you have to do is type [msfconsole] to start from scratch"
+sleep 2.0
+clear
+bash stop
+
